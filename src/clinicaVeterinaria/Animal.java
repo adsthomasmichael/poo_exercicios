b86874e4e968334a062a9469d5882e03c6bd3bd2
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-05-12 14:34:55 -0300

    Exercicio Clinica veterinaria. Classe Animal criada.

---
+++ src/clinicaVeterinaria/Animal.java
@@ -0,0 +1,37 @@
+package clinicaVeterinaria;
+
+public class Animal {
+
+    protected String nome;
+    protected int idade;
+
+    public Animal() {
+    }
+
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void imprimeInfo(){
+        System.out.println("=== ANIMAL ===");
+        System.out.println("Nome : "+ getNome());
+        System.out.println("Idade: "+ getIdade());
+    }
+}
