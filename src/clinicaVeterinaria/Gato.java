e93ef09d69f8b19f3dd2ec0215b1b1b62e7a52e9
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-05-12 15:31:09 -0300

    Classe Gato criada.

---
+++ src/clinicaVeterinaria/Gato.java
@@ -0,0 +1,47 @@
+package clinicaVeterinaria;
+
+public class Gato extends Animal{
+
+    protected double peso;
+    protected Consulta consulta;
+
+    public Gato(double peso, Consulta consulta) {
+        setPeso(peso);
+        this.consulta = consulta;
+    }
+
+    public Gato(String nome, int idade, double peso, Consulta consulta) {
+        super(nome, idade);
+        this.peso = peso;
+        this.consulta = consulta;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        if (peso > 0){
+            this.peso = peso;
+        }
+    }
+
+    public Consulta getConsulta() {
+        return consulta;
+    }
+
+    public void setConsulta(Consulta consulta) {
+        this.consulta = consulta;
+    }
+    public void imprimeConsulta(){
+        consulta.imprimeConsulta();
+    }
+
+    @Override
+    public void imprimeInfo(){
+        System.out.println("=== GATO ===");
+        super.imprimeInfo();
+        System.out.println("Peso: "+getPeso());
+        imprimeConsulta();
+    }
+}
