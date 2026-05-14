cc8e3998ccea242f264f044e41bdfd5f1a58c6dd
Michael Thomas <ads.thomasmichael@gmail.com>
2026-05-14 00:03:18 -0300

    finalização classe Teste

--- src/clinicaVeterinaria/Teste.java
+++ src/clinicaVeterinaria/Teste.java
@@ -1,5 +1,7 @@
 package clinicaVeterinaria;
 
+import com.utils.Teclado;
+
 public class Teste {
     public static void main(String[] args){
 
@@ -10,7 +12,28 @@ public class Teste {
         c01.imprimeInfo();
         g01.imprimeInfo();
 
-
+        consulta.setData(new Data(15,12,2026));
+        g01.imprimeConsulta();
+        
+        Animal a = null;
+        
+        int opcao = Teclado.leInt("Digite 1 para CACHORRO ou 2 para GATO: ");
+        
+        	if( opcao == 1) {
+        		a = new Cachorro(Teclado.leString("Qual o nome do Cachorro?"), Teclado.leInt("QUal a idade do cachorro?"), Teclado.leString("Qual a raça do cachorro?"));
+        		Cachorro ca1 = (Cachorro) a;
+        		ca1.setRaca("LAbrador");
+        	}
+        	else if( opcao == 2) {
+        		a = new Gato(Teclado.leString("Qual o nome do Gato?"), Teclado.leInt("Qual a idade do Gato"), Teclado.leDouble("Qual o peso do gato?"), consulta);
+        		Gato ga1 = (Gato) a;
+        		ga1.setPeso(6.1);
+        	}
+        	
+        	InformacoesAnimais info = new InformacoesAnimais();
+        	info.imprimeExclusivosAnimal(a);
+        	info.imprimeInformacoesAnimal(a);
+        	
 
     }
 }
