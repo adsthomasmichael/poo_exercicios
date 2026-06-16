faa60b5deb289e0a6bf46a888c65cc77c27d860c
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-06-16 18:09:40 -0300

    Classe TransportadoraNacional

---
+++ src/Transportadora/TransportadoraNacional.java
@@ -0,0 +1,33 @@
+package Transportadora;
+
+import java.util.Arrays;
+
+public class TransportadoraNacional extends EmpresaTransporte{
+
+    protected String registroANTT;
+
+    public TransportadoraNacional(String nome, String numCNPJ, double faturamentoBruto, double faturamentoLiquido, int qntMotoristas, String registroANTT) {
+        super(nome, numCNPJ, faturamentoBruto, faturamentoLiquido, qntMotoristas);
+        this.registroANTT = registroANTT;
+    }
+
+    public String getRegistroANTT() {
+        return registroANTT;
+    }
+
+    public void setRegistroANTT(String registroANTT) {
+        this.registroANTT = registroANTT;
+    }
+
+    @Override
+    public String toString() {
+        return "TransportadoraNacional{" +
+                "registroANTT='" + registroANTT + '\'' +
+                ", nome='" + nome + '\'' +
+                ", numCNPJ='" + numCNPJ + '\'' +
+                ", faturamentoBruto=" + faturamentoBruto +
+                ", faturamentoLiquido=" + faturamentoLiquido +
+                ", arrayMotoristas=" + Arrays.toString(arrayMotoristas) +
+                '}';
+    }
+}
